dc78ca7c1b50b6d0755b33ad2c841326e3af4477
peteratkau <59958667+peteratkau@users.noreply.github.com>
2025-09-30 09:34:44 +0200

    Update startgame.js

--- Workshop/W1/startgame.js
+++ Workshop/W1/startgame.js
@@ -38,3 +38,24 @@ class GameData {
     }
 
 }
+
+/*
+    1. Lägg till attributet milliseconds i GameData och tilldela den värdet 1000.
+    
+    2. Instansiera ett objekt ur klassen GameData och namnge det oGameData.
+    
+    2. Lägg till lyssnare för load
+    2.1. Lägg till lyssnare för keydown b eller B
+    2.1.1. Lägg till en timer.
+    2.1.2. Skapa ett img-element
+    2.1.3. Ändra Top, left, alt, src för img-elementet.
+    2.1.4. Ändra witdh, height, position för img-elementet.
+    2.1.5. Lägg till img-elementet till DOM:en.
+    2.1.6. Lägg till lyssnare på click på img-elementet
+
+    2.2. Lägg till lyssnare för keydown e eller E.
+    2.2.1. Avsluta spelet.
+    2.2.2. Visa information för spelaren.
+    2.2.3. Förbered för nytt spel.
+*/
+
